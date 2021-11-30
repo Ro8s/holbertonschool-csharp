cf89cefba5e91804563a2d0d41c42dc4a7de9345
Ro8s <rodrigo.delgado3132@gmail.com>
2021-11-30 13:45:40 -0300

    added ex 2

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char alp='a'; alp <= 'z'; alp++)
+                {
+                    Console.Write(alp);
+                }
+        }
+    }
+}
