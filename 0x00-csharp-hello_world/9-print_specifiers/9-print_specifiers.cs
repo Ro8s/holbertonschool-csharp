e61721a38d41c73e7cb8fb1c897dcb1189d07de9
Ro8s <rodrigo.delgado3132@gmail.com>
2021-11-29 22:08:14 -0300

    fix

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Globalization;
+
 class Program
 {
 	static void Main(string[] args)
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-        Console.WriteLine("Percent: {0}%\nCurrency: {1}", percent * 100, currency.ToString("C", CultureInfo.CurrentCulture));
+        Console.WriteLine("Percent: {0}\nCurrency: {1}", percent.ToString("0.00%"), currency.ToString("C", CultureInfo.CurrentCulture));
 	}
 }
