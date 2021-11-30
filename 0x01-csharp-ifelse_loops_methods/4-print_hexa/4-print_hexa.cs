11c47a39d1e8240c1172633f3412c32c8d0ccb07
Ro8s <rodrigo.delgado3132@gmail.com>
2021-11-30 14:09:46 -0300

    added ex 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int num = 0; num <= 98; num++)
+            {
+                Console.Write("{0} = {1}\n", num, String.Format("0x{0:x}", num));
+            }
+        }
+    }
+}
