711e2b88b30de8159036c3d8894307360f0450fd
Ro8s <rodrigo.delgado3132@gmail.com>
2021-11-29 21:52:25 -0300

    Added ex 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        Console.WriteLine("Percent: {0}%\nCurrency: {1}", percent * 100, currency.ToString("C", CultureInfo.CurrentCulture));
+	}
+}
