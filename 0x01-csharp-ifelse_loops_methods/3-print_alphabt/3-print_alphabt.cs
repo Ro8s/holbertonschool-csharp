8ebf6ec46ca8974a79580642b782130e117e2864
Ro8s <rodrigo.delgado3132@gmail.com>
2021-11-30 13:55:53 -0300

    added ex 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             for (char alp='a'; alp <= 'z'; alp++)
+                {
+                    if(alp != 'q' && alp != 'e')
+                    {
+                        Console.Write(alp);
+                    }
+                }
+        }
+    }
+}
