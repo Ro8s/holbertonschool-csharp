d310c557c0c40e44a5e1dc6f5767b4b5f6b805fe
Ro8s <rodrigo.delgado3132@gmail.com>
2021-11-30 16:50:31 -0300

    added ex 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n1 = 0; n1 <= 8; n1++)
+            {
+                for (int n2 = n1; n2 <= 9; n2++)
+                {
+                    if (n1 != n2)
+                    {
+                        Console.Write($"{n1}{n2}");
+                        if (n1 != 8)
+                        {
+                            Console.Write(", ");
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
