020557ef688bb4fc1ce43734ecd22a33d31554f1
Ro8s <rodrigo.delgado3132@gmail.com>
2021-11-29 23:47:13 -0300

    added ex 100

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+namespace _100_hello_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter errorWriter = Console.Error;
+            errorWriter.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+            System.Environment.Exit(1);
+        }
+    }
+}
