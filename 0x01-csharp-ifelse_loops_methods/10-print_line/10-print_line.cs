67ead129ece3b328589c9c1753ec5cbe19e12ceb
Ro8s <rodrigo.delgado3132@gmail.com>
2021-11-30 18:07:33 -0300

    added ex 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{    
+    public static void PrintLine(int length)
+    {
+        if (length > 0)
+        {
+            Console.WriteLine("{0}", new string('_', length));
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+    }
+}
+
