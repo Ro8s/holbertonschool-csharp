5df8ab8c3ba47961efb7cec9fdec7d63eeb8d8bd
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-01 16:20:57 -0300

    added ex 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return(null);
+        }
+        if (size == 0)
+        {
+            var emp = new List<int> {};
+            Console.WriteLine();
+            return(emp);
+        }
+
+        var lista = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            if (i + 1 == size)
+            {
+                Console.WriteLine("{0}", i);
+                lista.Add(i);
+            }
+            else
+            {
+                Console.Write("{0} ", i);
+                lista.Add(i);
+            }
+        }
+        return(lista);
+    }
+}
