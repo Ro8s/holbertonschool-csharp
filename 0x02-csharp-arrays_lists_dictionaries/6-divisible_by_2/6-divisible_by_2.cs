1ecbde24c14c2d774ef510b409c20426cec79876
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-01 20:38:01 -0300

    added ex 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        var lista = new List<bool>();
+        foreach (var item in myList)
+        {
+            if (item % 2 == 0)
+            {
+                lista.Add(true);
+            }
+            else
+            {
+                lista.Add(false);
+            }
+        }
+        return(lista);
+    }
+}
+
