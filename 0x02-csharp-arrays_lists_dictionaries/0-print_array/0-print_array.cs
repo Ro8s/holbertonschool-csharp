08599327268946c5511e032f620308e90c27ebb4
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-01 13:45:49 -0300

    added ex 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            int[] arr = new int[0];
+            Console.WriteLine();
+            return(arr);
+        }
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return(null);
+        }
+        int[] arr2 = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+
+            arr2[i] = i;
+            Console.Write("{0}", i);
+            if (i + 1 < size)
+            {
+                Console.Write(" ");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+        return(arr2);
+    }
+}
