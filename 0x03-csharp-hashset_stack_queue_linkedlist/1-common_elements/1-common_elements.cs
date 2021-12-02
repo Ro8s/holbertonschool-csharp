7b6e6548787a3da6fd40d79bc131385d9f878743
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-02 12:48:55 -0300

    added ex 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> uniq2 = new HashSet<int>(list2);
+        HashSet<int> inter = new HashSet<int>(list1);
+        inter.IntersectWith(uniq2);
+        List<int> res = new List<int>();
+        foreach (var item in inter)
+        {
+            res.Add(item);
+        }
+        return(res);
+
+    }
+}
+
