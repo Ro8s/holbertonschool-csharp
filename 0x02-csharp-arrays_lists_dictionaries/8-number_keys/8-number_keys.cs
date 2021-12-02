9dcfcd9a6e16daa0896e716ad45fc6b244db8084
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-01 21:22:40 -0300

    added ex 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int pichu = 0;
+        foreach (var item in myDict)
+        {
+            pichu++;
+        }
+        return (pichu);
+    }
+}
+
