864bdd7dee5a2a214303c2c09859b823b3cf83fd
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-01 21:14:41 -0300

    added ex 7

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index > myList.Count - 1 || index < 0)
+        {
+            Console.WriteLine("Index is out of range");
+            return(-1);
+        }
+        myList.Remove(myList[index]);
+        return(myList);
+    }
+}
