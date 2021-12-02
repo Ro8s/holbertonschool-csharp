2f5a2f12bae17519e3deffdf3c2c94fa8dc9bf75
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-02 11:52:56 -0300

    added ex 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+
+        var uniq = myList.Distinct();
+        int sum = 0;
+        foreach (int i in uniq)
+        {
+            sum += i;
+        }
+        return(sum);
+    }
+}
+
