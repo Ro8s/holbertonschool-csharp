a197317d70cb4eeb465e0392f8d8a047af3311d7
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-01 23:01:00 -0300

    added ex 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    if (i == 3 && j == 3)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else if (j == 5)
+                    {
+                        Console.WriteLine("0");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
