0a827fbe9dab8c2d873a1e76c50d8ab38622a477
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-01 21:16:35 -0300

    fix

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 
 class List
 {
@@ -9,7 +9,7 @@ class List
         if (index > myList.Count - 1 || index < 0)
         {
             Console.WriteLine("Index is out of range");
-            return(-1);
+            return(myList);
         }
         myList.Remove(myList[index]);
         return(myList);
