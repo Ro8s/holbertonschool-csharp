66184b3841345faa3ecaa789545600fe5edf8816
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-01 22:05:19 -0300

    added ex 11

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        var d = new Dictionary<string, int>();
+        
+        foreach (var item in myDict)
+        {
+            d[item.key] = item.Value * 2;
+        }
+        return (d);
+    }
+}
