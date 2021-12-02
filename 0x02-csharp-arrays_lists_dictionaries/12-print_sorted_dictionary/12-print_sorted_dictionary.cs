6534b3a800b4331ab4d1f41464e8549dd7bb90aa
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-01 22:32:29 -0300

    added ex 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var listakeys = myDict.Keys.ToList();
+        listakeys.Sort();
+        foreach (var item in listakeys)
+        {
+            Console.WriteLine("{0}: {1}", item, myDict[item]);
+        }
+        
+    }
+}
