9364abe375c7585b4daf613db548345a78429075
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-02 13:42:38 -0300

    added ex 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> uniq1 = new HashSet<int>(list1);
+        HashSet<int> uniq2 = new HashSet<int>(list2);
+        HashSet<int> exp = new HashSet<int>(list1);
+        exp.ExceptWith(uniq2);
+        uniq2.ExceptWith(uniq1);
+        List<int> res = new List<int>();
+        foreach (var item in exp)
+        {
+            res.Add(item);
+        }
+        foreach (var item in uniq2)
+        {
+            res.Add(item);
+        }
+        res.Sort();
+        return(res);
+    }
+}
+
