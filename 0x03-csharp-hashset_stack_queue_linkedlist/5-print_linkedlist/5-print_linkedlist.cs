4a2000146289484185f6a81674f5ec6a6e4a30c7
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-02 19:25:59 -0300

    added ex 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        List<int> lista = new List<int>();
+        LinkedList<int> empty = new LinkedList<int>();
+        if (size < 0)
+        {
+            return(empty);
+        }
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine("{0}", i);
+            lista.Add(i);
+        }
+        LinkedList<int> numbers = new LinkedList<int>(lista);
+        return(numbers);
+
+    }
+}
