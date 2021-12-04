5ea1655dac944f07ef2ad70a11e50544caab16de
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-04 15:19:54 -0300

    added ex 16

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jarray = new int[3][];
+            jarray[0] = new int[4] {0, 1, 2, 3};
+            jarray[1] = new int[7] {0, 1, 2, 3, 4, 5, 6};
+            jarray[2] = new int[2] {0, 1};
+            for (int i = 0; i < jarray.Length; i++)
+            {
+                for (int j = 0; j < jarray[i].Length; j++)
+                {
+                    if (j == jarray[i].Length - 1)
+                    {
+                        Console.WriteLine("{0}", jarray[i][j]);
+                    }
+                    else
+                    {
+                        Console.Write("{0} ", jarray[i][j]);
+                    }
+                }
+            }
+        }
+    }
+}
