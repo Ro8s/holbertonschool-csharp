01d883a0ccd3c1d2148a8a2411336df33d03fca0
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-04 15:36:09 -0300

    added ex 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newnode = myLList.First;
+        foreach (var node in myLList)
+        {
+            if (n > newnode.Value && n < newnode.Next.Value)
+            {
+                myLList.AddAfter(newnode, n);
+                break;
+            }
+            else
+            {
+                newnode = newnode.Next;
+            }
+            
+        }
+        return(newnode);
+
+    }
+}
+
