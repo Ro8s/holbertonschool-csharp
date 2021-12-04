7365783709f352f99582e503d01d4b29d933ef92
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-04 15:28:24 -0300

    fixed

--- 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -5,7 +5,7 @@ class LList
 {
     public static int GetNode(LinkedList<int> myLList, int n)
     {
-        if (n > myLList.Count - 1 || n <= 0)
+        if (n > myLList.Count - 1 || n < 0)
         {
             return(0);
         }
