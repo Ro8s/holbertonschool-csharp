ec53e0bd42f63e4e438b934453dc0c2d4f8371cf
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-04 18:33:33 -0300

    added ex 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (myLList.First == null)
+        {
+            return;
+        }
+        LinkedListNode<int> node = myLList.First;
+        for (int i = 0; i < myLList.Count; i++)
+        {
+            if (i == index)
+            {
+                myLList.Remove(node);
+                return;
+            }
+            else
+            {
+                node = node.Next;
+            }
+        }
+        return;
+    }
+}
