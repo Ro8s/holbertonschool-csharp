d21f03051203a38b1f4e19da15dbd4432693f976
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-04 13:11:18 -0300

    fixed

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _14_rectangular_array
 {
@@ -6,24 +7,28 @@ namespace _14_rectangular_array
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 5; i++)
+            int[,] matriz = new int[5,5];
+            for (int i = 0; i < 5; i++)
             {
-                for (int j = 0; j <= 5; j++)
+                for (int j = 0; j < 5; j++)
                 {
                     if (i == 2 && j == 2)
                     {
                         Console.Write("1 ");
+                        matriz[2,2] = 1;
                     }
-                    else if (j == 5)
+                    else if (j == 4)
                     {
                         Console.WriteLine("0");
+                        matriz[i,j] = 0;
                     }
                     else
                     {
                         Console.Write("0 ");
+                        matriz[i,j] = 0;
                     }
                 }
-            }
+            }  
         }
     }
 }
