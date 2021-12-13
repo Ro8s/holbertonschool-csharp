5b1045a0a27718074d88df710c685cd0f2229899
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-13 12:39:45 -0300

    added ex 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int pichu = 0;
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                pichu++;
+            }
+        }
+        catch (System.ArgumentOutOfRangeException)
+        {
+            return(pichu);
+        }
+        return(pichu);
+    }
+}
