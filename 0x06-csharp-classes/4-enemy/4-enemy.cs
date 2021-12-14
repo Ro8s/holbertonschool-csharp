48743634624d8ef90690253c8f03320bb9e478d4
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-14 19:28:57 -0300

    added ex 4

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Enemies
+{
+
+    ///<summary>Class Zombie. Defines health and a construct set to 0</summary>
+    public class Zombie
+    {
+        ///<summary>Health field</summary>
+        private int health;
+        ///<summary>Name field</summary>
+        private string name = "(No name)";
+
+        ///<summary>Public constructor <see cref="Zombie"/> class</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary> Public constructor <see cref="Zombie"/> class that recives a parametrer that changes de value of health</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+        ///<summary>Returns the value of health of the Zombie object.</summary>
+        public int GetHealth()
+        {
+            return(health);
+        }
+
+        ///<summary>Gets or sets the name.</summary>
+        public string Name
+        {
+            get {return(name);}
+            set {name = value;}
+        }
+
+    }
+}
