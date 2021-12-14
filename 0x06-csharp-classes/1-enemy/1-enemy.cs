97443f503b5178435c5724d06e8e062ffd409e76
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-14 16:28:49 -0300

    added ex 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+
+    ///<summary>Class Zombie. Defines health and a construct set to 0</summary>
+    public class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    }
+}
