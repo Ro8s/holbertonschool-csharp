cb1fa54edcdefda1cf15bc6d35da73bf4a75a9af
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-14 15:16:47 -0300

    fix

--- 0x06-csharp-classes/0-enemy/0-enemy.cs
+++ 0x06-csharp-classes/0-enemy/0-enemy.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
+///<summary>Create a new namespace Enemies.</summary>
+
 namespace Enemies
 {
+
+    ///<summary>Empty public class Zombie within Enemies that defines a zombie.</summary>
     public class Zombie
     {
     }
