6edc31ca8948fd48d722ad60c66ba0a7f7e08505
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-20 18:22:07 -0300

    added ex 2

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Obj
+{
+	///<summary>Checks if derivedType is a sublcass of basetype</summary>
+	public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+	{
+		if (derivedType.IsSubclassOf(baseType))
+        {
+			return (true);
+        }
+		return (false);
+	}
+}
