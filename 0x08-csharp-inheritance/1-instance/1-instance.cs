a988e25f05ccc4958612a59ccdd5eac8ecb3165c
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-20 16:42:58 -0300

    added ex 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Obj
+{
+	///<summary>Checks if obj is an instance of Array</summary>
+	public static bool IsInstanceOfArray(object obj)
+	{
+		if (obj is Array)
+		{
+			return (true);
+		}
+		return (false);
+	}
+}
