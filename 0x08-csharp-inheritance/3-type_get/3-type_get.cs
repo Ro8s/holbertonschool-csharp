1c6b60d3c320c2c131f0912d539280bf84cbd08c
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-20 19:08:24 -0300

    added ex 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+class Obj
+{
+	///<summary>method that prints the names of the available properties and methods of myObj</summary>
+	public static void Print(object myObj)
+	{
+		Console.WriteLine(myObj.GetType().Name + " Properties:");
+		PropertyInfo[] mypropinfo;
+		mypropinfo = myObj.GetType().GetProperties();
+		for (int i = 0; i < mypropinfo.Length; i++)
+        {
+			Console.WriteLine(mypropinfo[i].Name);
+        }
+		Console.WriteLine(myObj.GetType().Name + " Methods:");
+		MethodInfo[] mymethodinfo;
+		mymethodinfo = myObj.GetType().GetMethods();
+		for (int i = 0; i < mymethodinfo.Length; i++)
+		{
+			Console.WriteLine(mymethodinfo[i].Name);
+		}
+	}
+}
