6470553ecd3a2cda58b5cab863758288d94946bc
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-20 20:27:58 -0300

    added ex 5

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Shape
+{
+	/// <summary> method that throw a NotImplementedException </summary>
+	public virtual int Area()
+	{
+		throw new NotImplementedException("Area() is not implemented");
+	}
+}
