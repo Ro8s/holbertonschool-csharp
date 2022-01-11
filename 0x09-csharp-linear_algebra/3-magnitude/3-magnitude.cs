7c4d3e833817858c3986213b343a03a14d4d0222
Ro8s <rodrigo.delgado3132@gmail.com>
2022-01-11 12:53:02 -0300

    added ex 0-5

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class VectorMath
+{
+	public static double Magnitude(double[] vector)
+	{
+		if (vector.Length < 2 || vector.Length > 3)
+        {
+			return (-1);
+        }
+		double res = 0;
+		double op;
+		foreach (double val in vector)
+        {
+			op = val * val;
+			res = res + op;
+        }
+		return (Math.Round(Math.Sqrt(res), 2);
+	}
+}
