d6f907ddd951a9272a00b82e72834d3b14f1ef69
Ro8s <rodrigo.delgado3132@gmail.com>
2022-01-12 13:54:24 -0300

    added ex 8 to 13

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+/// <summary>
+/// VectorMath method
+/// </summary>
+class VectorMath
+{
+	/// <summary>
+	/// Method that calculates dot product of either two 2D or two 3D vectors.
+	/// </summary>
+	/// <param name="vector1">2D or 3D vector</param>
+	/// <param name="vector2">2D or 3D vector</param>
+	/// <returns>Dot product between vector1 and vector2</returns>
+	public static double DotProduct(double[] vector1, double[] vector2)
+	{
+		if (vector1.Length != vector2.Length)
+        {
+			return (-1);
+        }
+		if (vector1.Length < 2 || vector1.Length > 3)
+        {
+			return (-1);
+        }
+		if (vector2.Length < 2 || vector2.Length > 3)
+        {
+			return (-1);
+        }
+		double res = 0;
+		double op;
+		for (int i = 0; i < vector1.Length; i++)
+        {
+			op = vector1[i] * vector2[i];
+			res += op;
+        }
+		return (res);
+	}
+}
---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// VectorMath class
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Method that multiplies a vector and a scalar
+    /// </summary>
+    /// <param name="vector"> vector</param>
+    /// <param name="scalar"> scalar</param>
+    /// <returns>Multiplied vector</returns>
+    public static double[] Multiply(double[] vector, double scalar)
+	{
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return (-1);
+        }
+        double[] res = new double[vector.Length];
+        for(int i = 0; i < vector.Length; i++)
+        {
+            res[i] = vector[i] * scalar;
+        }
+        return (res);
+	}
+}
+
