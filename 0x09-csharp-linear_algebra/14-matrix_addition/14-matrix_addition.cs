7f5cbce4fe9115e3b7eb37f5fa3649be5d5c9d97
Ro8s <rodrigo.delgado3132@gmail.com>
2022-01-17 11:28:19 -0300

    added ex 14

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// MatrixMath class
+/// </summary>
+class MatrixMath
+{
+	/// <summary>
+	/// method that adds two matrix
+	/// </summary>
+	/// <param name="matrix1">First matrix to add</param>
+	/// <param name="matrix2">Second matrix to add</param>
+	/// <returns>The sum between matrix1 and matrix2</returns>
+	public static double[,] Add(double[,] matrix1, double[,] matrix2)
+	{
+		double[,] error = new double[,] { { -1 } };
+		if (matrix1.Length != matrix2.Length)
+		{
+			return (error);
+		}
+		if (matrix1.Length != 4 && matrix1.Length != 9)
+		{
+			return (error);
+		}
+		if (matrix2.Length != 4 && matrix2.Length != 9)
+		{
+			return (error);
+		}
+		double[,] res = matrix1;
+		for (int i = 0; i < matrix1.Rank; i++)
+        {
+			for (int j = 0; j < matrix1.GetLength(i); j++)
+            {
+				res[i, j] += matrix2[i, j];
+            }
+        }
+		return (res);
+	}
+}
