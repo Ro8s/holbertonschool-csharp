3da2fb2e9902ef52caa2db53fcf67e5933fc0d7b
Ro8s <rodrigo.delgado3132@gmail.com>
2022-03-21 14:44:43 -0300

    added ex 0

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Queue class
+/// </summary>
+/// <typeparam name="T">Type of elements inside the queue</typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// Function that returns the type that contains the queue
+    /// </summary>
+    /// <returns>Type of the element</returns>
+	public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
