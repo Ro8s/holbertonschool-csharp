47eed5b6c56d02102446e2162d759afe938c8b54
Ro8s <rodrigo.delgado3132@gmail.com>
2022-03-22 17:10:57 -0300

    added ex 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+/// <summary>
+/// Abstarct Base class
+/// </summary>
+public abstract class Base
+{
+	/// <summary>
+    /// name string property
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Override ToString Method toreturn an specific output
+    /// </summary>
+    /// <returns>[name] is a [type of class]</returns>
+    public override string ToString()
+    {
+        return this.name + " is a " + this.GetType(); 
+    }
+}
+
+/// <summary>
+/// IInteractive interface
+/// </summary>
+public interface IInteractive
+{ 
+    void Interact();
+}
+
+/// <summary>
+/// Ibreakable interface
+/// </summary>
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>
+/// ICollectable interface
+/// </summary>
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary>
+/// TestObject class that inherts Base and all interfaces crated
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{ 
+    /// <summary>
+    /// durability int property
+    /// </summary>
+    public int durability { get; set; }
+    void IInteractive.Interact()
+    {
+
+    }
+
+    void IBreakable.Break()
+    {
+
+    }
+    void ICollectable.Collect()
+    {
+
+    }
+    /// <summary>
+    /// isCollected bool property
+    /// </summary>
+    public bool isCollected { get; set; }
+
+}
