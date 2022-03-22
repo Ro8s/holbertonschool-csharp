5dc72530e3ae9f6841375852bb0c871dc1a59c8b
Ro8s <rodrigo.delgado3132@gmail.com>
2022-03-21 23:54:58 -0300

    added not finished ex

---
+++ 0x0A-csharp-generics/4-print/queue.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+/// <summary>
+/// Queue class
+/// </summary>
+/// <typeparam name="T">Type of elements inside the queue</typeparam>
+class Queue<T>
+{
+    public Node head { get; set; }
+    public Node tail { get; set; }
+    public int count;
+    
+    /// <summary>
+    /// Function that returns the type that contains the queue
+    /// </summary>
+    /// <returns>Type of the element</returns>
+	public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// Node class that defines nodes
+    /// </summary>
+    public class Node
+    {
+        public T value { get; set; }
+        public Node next { get; set; }
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+
+    }
+    /// <summary>
+    /// Enqueue method that enqueue an element into a queue
+    /// </summary>
+    /// <param name="element">element to add into the queue</param>
+    public void Enqueue(T element)
+    {
+        Node node = new Node(element);
+        node.value = element;
+        if (head == null)
+        {
+            node.next = null;
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            node.next = head;
+            head = node;
+        }
+        count++;
+    }
+    /// <summary>
+    /// Count method to count the elements into the queue
+    /// </summary>
+    /// <returns>Number of nodes</returns>
+    public int Count()
+    {
+        return count;
+    }
+    /// <summary>
+    /// Dequeue method that deletes an element from the queue
+    /// </summary>
+    /// <returns>value of deleted node</returns>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return (default(T));
+        }
+        Node aux = head;
+        T val1;
+        if (aux.next == null)
+        {
+
+            val1 = aux.value;
+            head = null;
+            tail = null;
+            count--;
+            return (val1);
+        }
+        while (aux.next != tail)
+        {
+            aux = aux.next;
+        }
+        T val = tail.value;
+        tail = null;
+        aux.next = tail;
+        count--;
+        return (val);
+
+    }
+    /// <summary>
+    /// Function that returns the peek of the queue
+    /// </summary>
+    /// <returns>value of the queue</returns>
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return (default(T));
+        }
+        T val = tail.value;
+        return (val);
+    }
+    /// <summary>
+    /// Function that prints all the queue starting from the peek
+    /// </summary>
+    public void Print()
+    {
+        Node aux = head;
+        Node aux2 = tail;
+        int c = 0;
+        if (head == null || tail == null)
+        {
+            Console.WriteLine("Queue is empty")
+        }
+        //while (c != count)
+        //{
+        //    while (aux != aux2.next)
+        //    {
+        //        if (aux.next == aux2)
+        //        {
+        //            console.writeline(aux2.value);
+        //            aux2 = aux;
+        //            break;
+        //        }
+        //        else
+        //        {
+        //            aux = aux.next;
+        //        }
+        //        aux = head;
+        //        c++;
+        //    }
+
+        //}
+    }
+}
