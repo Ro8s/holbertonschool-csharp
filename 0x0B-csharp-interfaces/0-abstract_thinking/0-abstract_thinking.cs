69826e4e058eff89f9b2b639cff4880cf80be9b9
Ro8s <rodrigo.delgado3132@gmail.com>
2022-03-22 15:44:06 -0300

    added ex 0

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Abstarct Base class
+/// </summary>
+public abstract class Base
+{
+	public string name { get; set; }
+
+
+    /// <summary>
+    /// Override ToString Method toreturn an specific output
+    /// </summary>
+    /// <returns>[name] is a [type of class]</returns>
+    public override string ToString()
+    {
+        return this.name + " is a " + this.GetType(); 
+    }
+
+}
