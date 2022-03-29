261ffcfbd31d19a11dc6111b8aab7893efa3f0e9
Ro8s <rodrigo.delgado3132@gmail.com>
2022-03-29 17:58:05 -0300

    added coments

--- 0x0D-csharp-text_based_interface/InventoryManagement/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryManagement/InventoryLibrary/BaseClass.cs
@@ -2,10 +2,22 @@
 
 namespace InventoryLibrary
 {
+    /// <summary>
+    /// BaseClass class
+    /// </summary>
     public class BaseClass
     {
+        /// <summary>
+        /// string id
+        /// </summary>
         public string id { get; set; }
+        /// <summary>
+        /// datecreated datetime
+        /// </summary>
         public DateTime date_created { get; set; }
+        /// <summary>
+        /// dateUpdated datetime
+        /// </summary>
         public DateTime date_updated { get; set; }
     }
 }
